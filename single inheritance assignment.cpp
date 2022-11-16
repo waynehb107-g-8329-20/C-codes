e5f06817e260b7566bafab85d565588f1f03eb03
waynehb107-g-8329-20 <114147201+waynehb107-g-8329-20@users.noreply.github.com>
2022-11-16 23:55:31 +0300

    Add files via upload

---
+++ single inheritance assignment.cpp
@@ -0,0 +1,25 @@
+//single inheritance program
+#include<iostream>
+using namespace std;
+class king{
+    public:
+    void land (){
+        cout<<"I have a castle"<<endl;
+    }
+    void car(){
+        cout<<"I own land"<<endl;
+    }
+};
+class prince: public king{
+    public:
+    void son(){
+        cout<<"I have two sons!"<<endl;
+    }
+};
+int main(){
+prince queen();
+queen.castle()
+queen.land();
+queen.son();
+return 0;
+}
